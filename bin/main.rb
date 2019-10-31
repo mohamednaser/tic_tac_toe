0d974ea02928c0e0f7d15a00681aaaa6422023cf
Santiago <santo1996.29@gmail.com>
2019-10-31 18:14:15 -0400

    user_interface by scenarios

--- bin/main.rb
+++ bin/main.rb
@@ -4,10 +4,10 @@
 puts 'Welcome to Tic Tac Toe game!'
 puts 'Developed by @mohamed_naser and @santiago-rodrig'
 puts
-puts 'First Player : Enter Your Name '
+print 'First player, please enter your name: '
 first_player_name = gets.strip
 
-puts 'Choose your symbol! X or O'
+print 'Choose your symbol! X or O: '
 first_player_symbol = gets.strip.downcase
 
 while first_player_symbol != 'x' && first_player_symbol != 'o'
@@ -21,8 +21,8 @@ second_player_name = gets.strip
 
 second_player_symbol = first_player_symbol == 'x' ? 'o' : 'x'
 
-puts "welcome #{first_player_name} with symbol (#{first_player_symbol}) and #{second_player_name} with symbol (#{second_player_symbol}) to our tic toc game"
-
+puts "Welcome #{first_player_name}, your symbol is: #{first_player_symbol}"
+puts "Welcome #{second_player_name}, your symbol is: #{second_player_symbol}"
 
 board = <<STRING
  * | * | *
@@ -30,27 +30,50 @@ board = <<STRING
  * | * | *
 STRING
 
-# This has no logic! Read the milestone 2! This is just a mockup...
-# This loop is just an example, we don't want an infinite loop
-6.times do |i|
-  loop do
-    if i % 2 != 0 
-      puts "#{first_player_name} : Choose your number! 1-9"
-    else
-      puts "#{second_player_name} : Choose your number! 1-9"
-    end
-    choice = gets.strip.to_i
-    break if (1..9).include? choice
-  end
-  puts 'Your choice is valid! printing current state of board...'
-  puts "The board looks like:"
-  puts board
-  # check if the game is over
-  # check if it is a draw
-  # if it is not a draw then it is a win scenario for the current player
-  # change the symbols
-  # increment turns
-end
+print "There are two possible scenarios: draw or winner, pick one: "
+scenario = gets.chomp.downcase
 
-puts "Well done #{second_player_name} you are the winner "
-puts 'Game over!'
+case scenario
+when 'winner'
+  6.times do |i|
+    loop do
+      if i % 2 != 0 
+        puts "#{first_player_name} : Choose your number! 1-9"
+      else
+        puts "#{second_player_name} : Choose your number! 1-9"
+      end
+      choice = gets.strip.to_i
+      break if (1..9).include? choice
+    end
+    puts 'Your choice is valid! printing current state of board...'
+    puts "The board looks like:"
+    puts board
+    # check if the game is over
+    # check if it is a draw
+    # if it is not a draw then it is a win scenario for the current player
+    # change the symbols
+  end
+  puts "Well done #{second_player_name} you are the winner "
+  puts 'Game over!'
+when 'draw'
+  8.times do |i|
+    loop do
+      if i % 2 != 0 
+        puts "#{first_player_name} : Choose your number! 1-9"
+      else
+        puts "#{second_player_name} : Choose your number! 1-9"
+      end
+      choice = gets.strip.to_i
+      break if (1..9).include? choice
+    end
+    puts 'Your choice is valid! printing current state of board...'
+    puts "The board looks like:"
+    puts board
+    # check if the game is over
+      # check if it is a draw
+      # if it is not a draw then it is a win scenario for the current player
+    # change the symbols
+  end
+  puts "There are no winners this time!"
+  puts 'Game over!'
+end
