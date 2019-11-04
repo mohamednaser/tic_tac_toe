39391189f12a376228161136a1df4d7680881136
Mohamed Naser <nc_m.naser@hotmail.com>
2019-11-04 22:23:55 +0200

    finish test cases for player test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,43 +3,47 @@
 require_relative '../lib/player.rb'
 
 describe Player do
+  describe '#initalize' do
+    let (:test_player_obj) { Player.new }
 
-  describe '.current' do
-    player = Player.new
+    context 'create first player ' do
+      it 'check current to be first when create a new player instance ' do
+        test_case = test_player_obj.class.current
+        expected_result = 'first'
 
-    it 'should read the current player created' do
-      expect(player.class.current).to be_truthy
-    end
-  end
-  describe '#initialize' do
-    let(:player) { Player.new }
-
-    context 'first player created' do
-      it "should set @@current to 'first'" do
-        expect(player_class.current).to eq('first')
+        expect(test_case).to eq(expected_result)
       end
     end
-    context 'second player created' do
-      let(:player_two) { Player.new }
 
-      it "should set @@current to 'second'" do
-        expect(player_class.current).to eq('second')
+    context 'second player object ' do
+      it 'check current to be second after initalize another player object ' do
+        test_case = test_player_obj.class.current
+        expected_result = 'second'
+
+        expect(test_case).to eq(expected_result)
       end
     end
   end
-  
-  let(:player) { Player.new }
 
+  let(:first_player_obj) { Player.new }
   describe '#name' do
-    it 'should allow to read and set the name' do
-      player.name = 'mohamed'
-      expect(player.name).to eq('mohamed')
+    it 'check set player name correctly ' do
+      test_case = first_player_obj.name = 'mohamed'
+      expected_result = 'mohamed'
+
+      expect(test_case).to eql(expected_result)
     end
   end
+
   describe '#symbol' do
-    it 'should allow to set and read the symbol' do
-      player.symbol = :x
-      expect(player.symbol).to eq(:x)
+    it 'check set player symbol correctly ' do
+      first_player_obj.name = 'mohamed'
+      first_player_obj.symbol = :x
+
+      test_case = first_player_obj.symbol
+      expected_result = :x
+
+      expect(test_case).to eql(expected_result)
     end
   end
 end
