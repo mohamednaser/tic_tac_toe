a395b38ed554f0d730bc80cd47c67fb51947f9cd
Santiago <santo1996.29@gmail.com>
2019-11-04 15:48:18 -0400

    tests for player class stage 2

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,42 +3,43 @@
 require_relative '../lib/player.rb'
 
 describe Player do
-  let(:player_one) { Player.new }
 
-  describe '#name' do
-    it 'should allow to read the name' do
-      player_one.name = 'mohamed'
-      expect(player_one.name).to eq('mohamed')
-    end
-  end
-  describe '#name=' do
-    it 'should allow to set the name' do
-      expect(player_one.name = 'santiago').to eq('santiago')
-    end
-  end
-  describe '#symbol' do
-    it 'should allow to read the symbol' do
-      player_one.symbol = :x
-      expect(player_one.symbol).to eq(:x)
-    end
-  end
-  describe '#symbol=' do
-    it 'should allow to set the symbol' do
-      expect(player_one.symbol = :o).to eq(:o)
+  describe '.current' do
+    player = Player.new
+
+    it 'should read the current player created' do
+      expect(player.class.current).to be_truthy
     end
   end
   describe '#initialize' do
+    let(:player) { Player.new }
+
     context 'first player created' do
       it "should set @@current to 'first'" do
+        expect(player_class.current).to eq('first')
       end
     end
     context 'second player created' do
       let(:player_two) { Player.new }
 
       it "should set @@current to 'second'" do
+        expect(player_class.current).to eq('second')
       end
     end
   end
-  describe '.current' do
+  
+  let(:player) { Player.new }
+
+  describe '#name' do
+    it 'should allow to read and set the name' do
+      player.name = 'mohamed'
+      expect(player.name).to eq('mohamed')
+    end
+  end
+  describe '#symbol' do
+    it 'should allow to set and read the symbol' do
+      player.symbol = :x
+      expect(player.symbol).to eq(:x)
+    end
   end
 end
