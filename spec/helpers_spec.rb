11c7c5b9dc0293d3c245cffb5bd5c8c2d4294196
Santiago <santo1996.29@gmail.com>
2019-11-04 18:37:22 -0400

    validations tests completed

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -14,18 +14,22 @@ describe Helpers do
       end
     end
     describe '.valid_move?' do
-      let(:player_one) { Player.new }
+      let(:player_one)  { Player.new }
       let(:player_two) { Player.new(:o) }
       let(:game) { Game.new(player_one, player_two) }
       it 'validates a correct move' do
         expect(Helpers::Validations.valid_move?(game.board, 1)).to be_truthy
+        game.make_move(1)
       end
       it 'invalidates a letter' do
         expect(Helpers::Validations.valid_move?(game.board, 'asd')).to be_falsy
       end
       it 'invalidates a noninteger number' do
+        expect(Helpers::Validations.valid_move?(game.board, 9.7)).to be_falsy
+        expect(Helpers::Validations.valid_move?(game.board, 4.1)).to be_falsy
       end
       it 'invalidates a position that is already taken' do
+        expect(Helpers::Validations.valid_move?(game.board, 1)).to be_falsy
       end
     end
   end
