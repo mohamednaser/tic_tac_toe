0291888eb466bd1c176a97ca8a46f7d167120cec
Santiago <santo1996.29@gmail.com>
2019-11-07 10:48:47 -0400

    refactored game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,22 +5,26 @@ require_relative '../lib/game.rb'
 
 describe Game do
   let(:first) { Player.new }
-  let(:second) { Player.new(:o) }
-  let(:game) { Game.new(first, second) }
+  let(:second) { Player.new }
+  before do
+    first.symbol = :x
+    second.symbol = :o
+    @game = Game.new first, second
+  end
 
   describe '#initialize' do
     it 'sets @board to a 3 by 3 matrix with nils' do
-      all_nil = game.board.all? { |row| row.all?(NilClass) }
+      all_nil = @game.board.all? { |row| row.all?(NilClass) }
       expect(all_nil).to be_truthy
     end
     it 'sets @player to @first' do
-      expect(game.player.symbol).to eq(:x)
+      expect(@game.player).to eq(first)
     end
     it 'sets @moves to 0' do
-      expect(game.moves).to eq(0)
+      expect(@game.moves).to eq(0)
     end
     it 'sets @state to :playing' do
-      expect(game.state).to eq(:playing)
+      expect(@game.state).to eq(:playing)
     end
   end
 
@@ -28,29 +32,29 @@ describe Game do
     it 'gives a string representing the board' do
       board_as_string_zero = " -  -  - \n -  -  - \n -  -  - \n"
       board_as_string_one = " x  x  - \n -  o  - \n -  x  o \n"
-      expect(game.board_string).to eq(board_as_string_zero)
+      expect(@game.board_string).to eq(board_as_string_zero)
       moves = [1, 5, 2, 9, 8]
-      moves.each { |move| game.make_move(move) }
+      moves.each { |move| @game.make_move(move) }
 
-      expect(game.board_string).to eq(board_as_string_one)
+      expect(@game.board_string).to eq(board_as_string_one)
     end
   end
 
   describe '#playing' do
     it 'gives false if @state is not equal to :playing' do
-      game.state = :winner
-      expect(game.playing).to be_falsy
+      @game.state = :winner
+      expect(@game.playing).to be_falsy
     end
     it 'gives true if @state is equal to :playing' do
-      expect(game.playing).to be_truthy
+      expect(@game.playing).to be_truthy
     end
   end
 
   describe '#swap_players' do
     it 'check swap players after success move' do
-      current_player = game.player
-      game.make_move(3)
-      expect(game.player).not_to eq(current_player)
+      current_player = @game.player
+      @game.make_move(3)
+      expect(@game.player).not_to eq(current_player)
     end
   end
 
@@ -58,17 +62,17 @@ describe Game do
     context '#winner?' do
       it 'changes the state to winner' do
         moves = [1, 5, 2, 9, 8, 4, 3]
-        moves.each { |move| game.make_move(move) }
+        moves.each { |move| @game.make_move(move) }
 
-        expect(game.state).to eql(:winner)
+        expect(@game.state).to eql(:winner)
       end
     end
     context 'having a draw' do
       it 'changes the state to draw' do
         moves = [1, 5, 3, 7, 4, 2, 8, 6, 9]
-        moves.each { |move| game.make_move(move) }
+        moves.each { |move| @game.make_move(move) }
 
-        expect(game.state).to eql(:draw)
+        expect(@game.state).to eql(:draw)
       end
     end
   end
--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -10,30 +10,41 @@ describe Helpers do
       it 'validates correct symbols' do
         expect(Helpers::Validations.valid_symbol?(:x)).to be_truthy
       end
+
       it 'invalids wrong symbols' do
         expect(Helpers::Validations.valid_symbol?(:fgf)).to be_falsy
       end
     end
+
     describe '.valid_move?' do
-      let(:player_one) { Player.new }
-      let(:player_two) { Player.new(:o) }
-      let(:game) { Game.new(player_one, player_two) }
+      let(:first) { Player.new }
+      let(:second) { Player.new }
+      before do
+        first.symbol = :x
+        second.symbol = :o
+        @game = Game.new first, second
+      end
+
       it 'validates a correct move' do
-        expect(Helpers::Validations.valid_move?(game.board, 1)).to be_truthy
+        expect(Helpers::Validations.valid_move?(@game.board, 1)).to be_truthy
       end
+
       it 'invalidates a letter' do
-        expect(Helpers::Validations.valid_move?(game.board, 'asd')).to be_falsy
+        expect(Helpers::Validations.valid_move?(@game.board, 'asd')).to be_falsy
       end
+
       it 'invalidates a noninteger number' do
-        expect(Helpers::Validations.valid_move?(game.board, 9.7)).to be_falsy
-        expect(Helpers::Validations.valid_move?(game.board, 4.1)).to be_falsy
+        expect(Helpers::Validations.valid_move?(@game.board, 9.7)).to be_falsy
+        expect(Helpers::Validations.valid_move?(@game.board, 4.1)).to be_falsy
       end
+
       it 'invalidates a position that is already taken' do
-        game.make_move(1)
-        expect(Helpers::Validations.valid_move?(game.board, 1)).to be_falsy
+        @game.make_move(1)
+        expect(Helpers::Validations.valid_move?(@game.board, 1)).to be_falsy
       end
     end
   end
+
   describe Helpers::Mappers do
     describe '.map_number' do
       it 'maps correctly a valid number' do
@@ -42,6 +53,7 @@ describe Helpers do
         expect(Helpers::Mappers.map_number(7)).to eq(x: 2, y: 0)
         expect(Helpers::Mappers.map_number(9)).to eq(x: 2, y: 2)
       end
+
       it 'returns nil if the number is invalid' do
         expect(Helpers::Mappers.map_number(9.7)).to be_nil
         expect(Helpers::Mappers.map_number('123')).to be_nil
