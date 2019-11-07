c81d339eeefb19086053a311dd1160270bd98ab8
Santiago <santo1996.29@gmail.com>
2019-11-07 10:31:19 -0400

    refactored player tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,64 +3,53 @@
 require_relative '../lib/player.rb'
 
 describe Player do
-  describe '#initalize' do
-    let(:test_player_obj) { Player.new }
+  let(:player) { Player.new }
 
-    it 'check initalize with default symbol ' do
-      player_obj = Player.new
-
-      expect(player_obj.symbol).to eq(:x)
-    end
-
-    context 'initalize with argument ' do
-      it 'valid argument' do
-        player_obj = Player.new(:o)
-
-        expect(player_obj.symbol).to eq(:o)
-      end
-    end
-
-    context 'create first player ' do
-      before { Player.current = nil }
-      it 'check current to be first when create a new player instance ' do
-        test_case = test_player_obj.class.current
-        expected_result = 'first'
-
-        expect(test_case).to eq(expected_result)
-      end
-    end
-
-    context 'second player object ' do
-      before { Player.current = 'first' }
-      it 'check current to be second after initalize another player object ' do
-        test_case = test_player_obj.class.current
-        expected_result = 'second'
-
-        expect(test_case).to eq(expected_result)
+  context 'first player has been created' do
+    describe '#initialize' do
+      it "should set the current player to 'first'" do
+        expect(player.class.current).to eq('first')
       end
     end
   end
 
-  describe '#name' do
-    it 'check set player name correctly ' do
-      first_player_obj = Player.new
-      test_case = first_player_obj.name = 'mohamed'
-      expected_result = 'mohamed'
-
-      expect(test_case).to eql(expected_result)
+  context 'secon player has been created' do
+    describe '#initialize' do
+      it "should set the current player to 'second'" do
+        expect(player.class.current).to eq('second')
+      end
     end
   end
 
-  describe '#symbol' do
-    it 'check set player symbol correctly ' do
-      first_player_obj = Player.new
-      first_player_obj.name = 'mohamed'
-      first_player_obj.symbol = :x
+  context 'setting the name for the player' do
+    describe '#name' do
+      it 'should not raise NoMethodError' do
+        expect { player.name = 'some name' }.not_to raise_error(NoMethodError)
+      end
+    end
+  end
 
-      test_case = first_player_obj.symbol
-      expected_result = :x
+  context 'getting the name of the player' do
+    describe '#name' do
+      it 'should not raise NoMethodError' do
+        expect { player.name }.not_to raise_error(NoMethodError)
+      end
+    end
+  end
 
-      expect(test_case).to eql(expected_result)
+  context 'setting the symbol of the player' do
+    describe '#symbol' do
+      it 'should not raise NoMethodError' do
+        expect { player.symbol = :x }.not_to raise_error(NoMethodError)
+      end
+    end
+  end
+
+  context 'getting the symbol of the player' do
+    describe '#symbol' do
+      it 'should not raise NoMethodError' do
+        expect { player.symbol }.not_to raise_error(NoMethodError)
+      end
     end
   end
 end
